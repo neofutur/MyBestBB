cb7156c016710ffad2af88964c650561ef3e76a4
zapsoda <avofvt@gmail.com>
2012-08-13 23:55:12 -0500

    Added transaltions for Fourm_Cleanup and Profile_Fields plugins

---
+++ lang/Spanish/AP_Profile_Fields.php
@@ -0,0 +1,32 @@
+<?php
+
+$lang_Profile_Fields = array(
+
+'title'                 =>      'Easy Profile Fields',
+'description'           =>      'In this plugin you can easily create extra profile fields that will show up in viewtopic.php.',
+
+// Menu Items
+'MI_help'               =>      'Help',
+'MI_menu'               =>      'Menu',
+'MI_Hide'               =>      'Hide',
+
+// Options
+'Option_New'            =>      'Create a new user field',
+'Option_Edit'           =>      'Edit a user field',
+'Option_Delete'         =>      'Delete a user field',
+'Option_View'           =>      'View user fields',
+
+
+// Headers
+'H_CreateNewField'      =>      'Create New Profile Field',
+'H_DeleteField'			=>		'Delete Profile Field',
+'H_EditField'			=>		'Edit Profile Field',
+'H_ListFields'			=>		'List Profile Fields',
+
+// Form Labels
+'FL_Order'              =>      'Order:',
+'FL_Lang'               =>      'Language file entry:',
+'FL_Name'               =>      'Name:',
+'FL_Id'					=>		'Id:'
+
+);?>
